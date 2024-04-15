7b7f605458c05249555ef0b5477c84cd90758674
Mishany52 <kalintsev309206@gmail.com>
2024-04-15 12:03:21 +0300

    feat/315-create-route-get-account-by-id https://app.weeek.net/ws/460422/task/315 - исправил path согласно miro на /account/get-info/:id #315

--- src/api/http/controllers/account.controller.ts
+++ src/api/http/controllers/account.controller.ts
@@ -35,9 +35,8 @@ export class AccountController {
         return 'Hello World ' + req.accountId;
     }
 
-    @Get(':id')
+    @Get('get-info/:id')
     async findOne(@Param('id') id: UUID): Promise<GetAccountDto> {
-        console.log(id);
         return this._accountService.getAccount(id);
     }
 }
