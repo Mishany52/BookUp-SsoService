7a21a76f3a69638c770a1a6067ded782f02db5cc
Mishany52 <kalintsev309206@gmail.com>
2024-04-30 15:21:53 +0300

    feat/363-create-route-update-account-by-id https://app.weeek.net/ws/460422/task/363 - создал route для обновления данных пользователя - добавил validationPipe

--- src/domains/account/account.service.ts
+++ src/domains/account/account.service.ts
@@ -1,11 +1,11 @@
 import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
 import { Account } from '../../infrastructure/types/account';
-
 import { IAccountRepository } from './account.repository.interface';
 import * as argon2 from 'argon2';
 import { CreateAccountDto } from './dtos/create-account.dto';
 import { UUID } from 'crypto';
 import { GetAccountDto } from './dtos/get-account.dto';
+import { AccountUpdateDto } from './dtos/update-account.dto';
 
 const accountRepo = () => Inject('accountRepo');
 
@@ -50,5 +50,26 @@ export class AccountService {
     }
     async getAccounts(): Promise<Account[]> {
         return this._accountRepository.getAccounts();
+    }
+
+    async updateAccount(
+        accountId: UUID,
+        accountUpdateDto: Partial<AccountUpdateDto>,
+    ): Promise<GetAccountDto> {
+        const account = await this._accountRepository.getById(accountId);
+        if (!account) {
+            throw new HttpException('Account not found', HttpStatus.NOT_FOUND);
+        }
+        if (accountUpdateDto.password.length != 0) {
+            accountUpdateDto.password = await argon2.hash(accountUpdateDto.password);
+        }
+        // Обновляем поля сущности
+        Object.assign(account, accountUpdateDto);
+
+        const accountUpdate = await this._accountRepository.save(account);
+        if (!accountUpdate) {
+            throw new HttpException('Account update failed successfully', HttpStatus.BAD_REQUEST);
+        }
+        return new GetAccountDto(accountUpdate);
     }
 }
