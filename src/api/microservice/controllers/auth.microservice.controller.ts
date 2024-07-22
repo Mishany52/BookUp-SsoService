22b13f8c4becda0f33dcdef7470fe8a3577ee3bc
Mishany52 <kalintsev309206@gmail.com>
2024-07-22 07:33:10 +0300

    - Исправил расположение microservice controllers

--- src/api/microservice/controllers/auth.microservice.controller.ts
+++ src/api/microservice/controllers/auth.microservice.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, HttpStatus } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { SignUpDto } from '../dto/auth/sign-up.dto';
+import { SignUpDto } from 'src/api/http/controllers/dto/auth/sign-up.dto';
 import { AuthService } from 'src/domains/auth/auth.service';
 import { IAccountSingUpResponse } from 'src/domains/interface/account/account-sign-up.response.interface';
 import { SING_UP_BAD_REQUEST } from 'src/infrastructure/constants/microservice-messages/response-auth.messages';
-import { SING_UP_SUCCESS } from '../../../../infrastructure/constants/microservice-messages/response-auth.messages';
+import { SING_UP_SUCCESS } from 'src/infrastructure/constants/microservice-messages/response-auth.messages';
 
 @ApiTags('Authentication')
 @Controller('authMicroservice')
--- src/domains/account/account.module.ts
+++ src/domains/account/account.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AccountHttpController } from 'src/api/http/controllers/account/account.http.controller';
 import { AccountService } from './account.service';
 import { AccountRepositoryModule } from 'src/infrastructure/repository/account/account-repository.module';
-import { AccountMicroserviceController } from 'src/api/http/controllers/account/account.microservice.controller';
+import { AccountMicroserviceController } from 'src/api/microservice/controllers/account.microservice.controller';
 @Module({
     imports: [AccountRepositoryModule],
     controllers: [AccountHttpController, AccountMicroserviceController],
